44aedcb9e53f6bbd7b40ef149e0a336f9385a426
Joao Sa <me@joaomsa.com>
2014-08-23 18:15:40 -0300

    Add d3 donations graph

--- static/js/app.js
+++ static/js/app.js
@@ -41,6 +41,12 @@
                                 $scope.candidates[i].projeto.down)
                     }
                 }, 100);
+                setTimeout(function(){
+                    for (var i = 0; i < $scope.candidates.length; i++){
+                        drawDonations("donations_" + $scope.candidates[i].id, 
+                            $scope.candidates[i].id )
+                    }
+                }, 100);
             }, true);
         }
     ]);
@@ -129,6 +135,93 @@
         ctx.font = "20px Helvetica";
         if(up_vote > down_vote) ctx.fillText((up_vote/myTotal * 100 | 0) + " %", 180, 230);
         else ctx.fillText((down_vote/myTotal * 100 | 0) + " %", 180, 80);
+    }
+
+    function drawDonations(div_id, id){
+        var margin = {top: 20, right: 20, bottom: 30, left: 70},
+            width = 400 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], 0.1);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10,"$,");
+
+
+        var svg = d3.select("#" + div_id).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var drawUpdate = function(data){
+        };
+        d3.json(id + ".json", function(error, data) {
+            console.log(svg);
+            data.forEach(function (d) {
+            if(d.montante == null)
+            {d.montante = 0}
+            d.montante = parseInt(d.montante) // / 1000
+            })
+            
+            data = data.sort(function (a,b) {
+                if (a.montante < b.montante)
+                    return 1;
+                if (a.montante > b.montante)
+                    return -1;
+                return 0;
+                })
+
+            // set x and y limits
+            x.domain(data.map(function(d) { return d.nome; }));
+            y.domain([0, d3.max(data, function(d) { return d.montante; })]);
+
+            // Draw y axis
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                // .attr("transform", "rotate(-90)")
+                // .attr("y", 6)
+                // .attr("dy", ".71em")
+                // .style("text-anchor", "end")
+                // .text("Montante");
+
+            // Draw bars
+            svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.nome); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.montante); })
+            .attr("height", function(d) { return height - y(d.montante); });
+
+
+            // Draw x axis
+            svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("transform", function (d) {
+                return "translate(-10,-10) rotate(-90)"
+        //        return "translate(0,0) rotate(-90)"
+            })
+            .style("text-anchor","start");
+        
+        });
     }
 
 }());
